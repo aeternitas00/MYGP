489a7e82253fcc9bf5f3f6f0ecb522fa94c6ed41
YEHI-OR\Miningyeo <dlwjddn147@gmail.com>
2017-04-20 00:44:53 +0900

    no message

--- Default/Textures.cpp
+++ Default/Textures.cpp
@@ -31,7 +31,9 @@
 
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 #define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
-#define ACC_GRV 0.48f
+#define ACC_GRV 0.96f
+#define MIN_Y 290
+#define MIN_Y 290
 #pragma warning( disable : 4996 ) // disable deprecated warning 
 #include <strsafe.h>
 #pragma warning( default : 4996 )
@@ -45,13 +47,20 @@ LPDIRECT3D9             g_pD3D = NULL; // Used to create the D3DDevice
 LPDIRECT3DDEVICE9       g_pd3dDevice = NULL; // Our rendering device
 LPDIRECT3DVERTEXBUFFER9 g_pVB = NULL; // Buffer to hold vertices
 LPDIRECT3DTEXTURE9      g_pTexture = NULL; // Our texture
-LPDIRECT3DTEXTURE9      g_pTexture2 = NULL; // Our texture
+LPDIRECT3DTEXTURE9      g_pTexturebg = NULL; // Our texture
+LPDIRECT3DTEXTURE9      g_pTextureDust = NULL; // Our texture
 LPD3DXSPRITE	g_pd3dSprite = NULL;
+LPD3DXSPRITE	g_pd3dSpritebg = NULL;
+LPD3DXSPRITE	g_pd3dSpriteDust = NULL;
 D3DXIMAGE_INFO g_Img1;
+D3DXIMAGE_INFO g_Imgbg;
+D3DXIMAGE_INFO g_ImgDust;
 RECT g_ImgRc1;
+RECT g_ImgRcbg;
+RECT g_ImgRcDust;
 
-D3DXVECTOR3 pos(125, 183, 0);
-
+D3DXVECTOR3 pos(250, MIN_Y, 0);
+D3DXVECTOR3 dustpos(0, 0, 0);
 
 
 // A structure for our custom vertex type. We added texture coordinates
@@ -130,10 +139,10 @@ HRESULT InitGeometry()
         }
     }
 
-	if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, L"bg.bmp", &g_pTexture2)))
+	if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, L"bg.bmp", &g_pTexturebg)))
 	{
 		// If texture is not in current folder, try parent folder
-		if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, L"..\\bg.bmp", &g_pTexture2)))
+		if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, L"..\\bg.bmp", &g_pTexturebg)))
 		{
 			MessageBox(NULL, L"Could not find bg.bmp", L"Textures.exe", MB_OK);
 			return E_FAIL;
@@ -141,6 +150,26 @@ HRESULT InitGeometry()
 	}
 
 	if (FAILED(D3DXCreateSprite(g_pd3dDevice, &g_pd3dSprite)))
+	{
+		g_pd3dDevice->Release();
+		g_pd3dDevice = NULL;
+
+		g_pD3D->Release();
+		g_pD3D = NULL;
+
+		return E_FAIL;
+	}
+	if (FAILED(D3DXCreateSprite(g_pd3dDevice, &g_pd3dSpritebg)))
+	{
+		g_pd3dDevice->Release();
+		g_pd3dDevice = NULL;
+
+		g_pD3D->Release();
+		g_pD3D = NULL;
+
+		return E_FAIL;
+	}
+	if (FAILED(D3DXCreateSprite(g_pd3dDevice, &g_pd3dSpriteDust)))
 	{
 		g_pd3dDevice->Release();
 		g_pd3dDevice = NULL;
@@ -171,6 +200,48 @@ HRESULT InitGeometry()
 		return E_FAIL;
 	}::SetRect(&g_ImgRc1, 0, 0, g_Img1.Width, g_Img1.Height);
 
+
+	if (FAILED(D3DXCreateTextureFromFileEx(
+		g_pd3dDevice				// 디바이스 포인터
+		, L"bg.bmp"		// 텍스처 이름
+		, D3DX_DEFAULT
+		, D3DX_DEFAULT
+		, 1							// 밉 레벨(2D의 경우 반드시 1)
+		, 0
+		, D3DFMT_UNKNOWN
+		, D3DPOOL_MANAGED
+		, 0x0000001					// 필터링
+		, 0x0000001					// 밉 필터
+		, 0xFFFFFFFF				// 컬러 키
+		, &g_Imgbg					// 텍스처 인포메이션
+		, NULL
+		, &g_pTexturebg				// 텍스처 포인터
+		))) {
+		MessageBox(NULL, L"Could not find bg.bmp", L"Textures.exe", MB_OK);
+		return E_FAIL;
+	}::SetRect(&g_ImgRcbg, 0, 0, g_Imgbg.Width, g_Imgbg.Height);
+
+
+	if (FAILED(D3DXCreateTextureFromFileEx(
+		g_pd3dDevice				// 디바이스 포인터
+		, L"Sprite_FX_Dust.png"		// 텍스처 이름
+		, D3DX_DEFAULT
+		, D3DX_DEFAULT
+		, 1							// 밉 레벨(2D의 경우 반드시 1)
+		, 0
+		, D3DFMT_UNKNOWN
+		, D3DPOOL_MANAGED
+		, 0x0000001					// 필터링
+		, 0x0000001					// 밉 필터
+		, D3DCOLOR_XRGB(4,142,176)				// 컬러 키
+		, &g_ImgDust					// 텍스처 인포메이션
+		, NULL
+		, &g_pTextureDust					// 텍스처 포인터
+		))) {
+		MessageBox(NULL, L"Could not find Sprite_FX_Dust.png", L"Textures.exe", MB_OK);
+		return E_FAIL;
+	}::SetRect(&g_ImgRcDust, 0, 0, g_ImgDust.Width, g_ImgDust.Height);
+
     // Create the vertex buffer.
     if( FAILED( g_pd3dDevice->CreateVertexBuffer( 8 * sizeof( CUSTOMVERTEX ),
                                                   0, D3DFVF_CUSTOMVERTEX,
@@ -185,25 +256,6 @@ HRESULT InitGeometry()
     if( FAILED( g_pVB->Lock( 0, 0, ( void** )&pVertices, 0 ) ) )
         return E_FAIL;
 
-	pVertices[4].position = D3DXVECTOR3(-7.0f, 7.0f, 1.0f);
-		pVertices[4].color = 0xffffffff;
-		pVertices[4].tu = 0.0f;
-		pVertices[4].tv = 0.0f;
-
-		pVertices[5].position = D3DXVECTOR3(7.0f, 7.0f, 1.0f);
-		pVertices[5].color = 0xffffffff;
-		pVertices[5].tu = 1.0f;
-		pVertices[5].tv = 0.0f;
-
-		pVertices[6].position = D3DXVECTOR3(-7.0f, -7.0f, 1.0f);
-		pVertices[6].color = 0xffffffff;
-		pVertices[6].tu = 0.0f;
-		pVertices[6].tv = 1.0f;
-
-		pVertices[7].position = D3DXVECTOR3(7.0f, -7.0f, 1.0f);
-		pVertices[7].color = 0xffffffff;
-		pVertices[7].tu = 1.0f;
-		pVertices[7].tv = 1.0f;
     g_pVB->Unlock();
 	pVertices2=pVertices;
     return S_OK;
@@ -220,6 +272,12 @@ VOID Cleanup()
 {
 
 	if (g_pd3dSprite)
+		g_pd3dSprite->Release();
+
+	if (g_pd3dSpritebg)
+		g_pd3dSprite->Release();
+
+	if (g_pd3dSpriteDust)
 		g_pd3dSprite->Release();
 
     if( g_pTexture != NULL )
@@ -243,145 +301,36 @@ VOID Cleanup()
 //-----------------------------------------------------------------------------
 VOID SetupMatrices()
 {
-    // Set up world matrix
-	static bool go_left;
-	static bool go_right;
-	static bool moving;
-	static int stat = 0;
-	static float upvel = 0;
-	static bool jumping_up = false;
-	static bool jumping = false;
-	static bool jumping_tgl = false;
-	static bool increase = true;
-
-	moving = false; 
-
-	if (KEY_DOWN(VK_LEFT)) {
-		if(go_right)
-
-		go_left = true; go_right = false; moving = true;
-	}
-	if (KEY_DOWN(VK_RIGHT)) { 
-		go_right = true; go_left = false; moving = true;
-	}
-	if (KEY_DOWN(VK_UP)) {
-		if (!jumping&&!jumping_tgl){
-			jumping_tgl = true;
-			jumping = true; jumping_up = true; upvel = 3.0f;
-		}
-		else if(jumping) {
-			if (jumping_up)
-				upvel += 0.246;
-		}
-	}
-	if (jumping_tgl) { if (KEY_UP(VK_UP)) jumping_tgl = false; }
-
-	if (go_left&&go_right)
-		moving = false;
-
-	if (!moving&&!jumping)
-	{
-		g_ImgRc1.bottom = 32;
-		if (go_right) {
-			g_ImgRc1.left = 0;
-			g_ImgRc1.right = g_ImgRc1.left + 24;
-		}
-		else if (go_left) {
-			g_ImgRc1.left = 175;
-			g_ImgRc1.right = g_ImgRc1.left + 24;
-		}
-		else if (!go_right&&!go_left)
-		{
-			g_ImgRc1.left = 0;
-			g_ImgRc1.right = g_ImgRc1.left + 24;
-		}
-	}
-	else if(moving&&!jumping)
-	{
-		g_ImgRc1.bottom = 32;
-		if (go_right)
-		{
-			if (stat >= 0 && stat <= 7) {
-				g_ImgRc1.left = 25;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-			else if (stat >= 8 && stat <= 23) {
-				g_ImgRc1.left = 50;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-			else if (stat >= 24 && stat <= 31) {
-				g_ImgRc1.left = 75;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-		}
-		else if (go_left)
-		{
-			if (stat >= 0 && stat <= 7) {
-				g_ImgRc1.left = 150;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-			else if (stat >= 8 && stat <= 23) {
-				g_ImgRc1.left = 125;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-			else if (stat >= 24 && stat <= 31) {
-				g_ImgRc1.left = 100;
-				g_ImgRc1.right = g_ImgRc1.left + 24;
-			}
-		}
-	}
-	else if (jumping) {
-		g_ImgRc1.bottom = 32;
-		if (go_right){
-			g_ImgRc1.left = 200;
-			g_ImgRc1.right = g_ImgRc1.left + 29;
-		}
-		else if(go_left){
-			g_ImgRc1.left = 230;
-			g_ImgRc1.right = g_ImgRc1.left + 29;
-		}
-	}
-
-	if (stat >= 31) increase = false; else if (stat <= 0)increase = true;
-	if (increase)stat++; else stat--;
+	// Set up world matrix
 	
-	if (moving)
-	{
-		if (go_left) pos.x -= 1.0f; if (go_right) pos.x += 1.0f;
-	}
-	if (jumping)
-	{
-		if (upvel <= 0) jumping_up = false;
-		if (pos.y - upvel >= 183.0f){pos.y = 183.0f; jumping = false; /*먼지 스프라이트 추가*/}
-		else {pos.y -= upvel;upvel -= ACC_GRV;}
-	}
 
 
-    D3DXMATRIXA16 matWorld;
-    D3DXMatrixIdentity( &matWorld );
+	D3DXMATRIXA16 matWorld;
+	D3DXMatrixIdentity(&matWorld);
 	D3DXMatrixTranslation(&matWorld, 0, 0, 0);
-    g_pd3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
 
-    // Set up our view matrix. A view matrix can be defined given an eye point,
-    // a point to lookat, and a direction for which way is up. Here, we set the
-    // eye five units back along the z-axis and up three units, look at the
-    // origin, and define "up" to be in the y-direction.
-    D3DXVECTOR3 vEyePt( 0.0f, 0.0f,-12.0f );
-    D3DXVECTOR3 vLookatPt( 0.0f, 0.0f, 0.0f );
-    D3DXVECTOR3 vUpVec( 0.0f, 1.0f, 0.0f );
-    D3DXMATRIXA16 matView;
-    D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
-    g_pd3dDevice->SetTransform( D3DTS_VIEW, &matView );
+	// Set up our view matrix. A view matrix can be defined given an eye point,
+	// a point to lookat, and a direction for which way is up. Here, we set the
+	// eye five units back along the z-axis and up three units, look at the
+	// origin, and define "up" to be in the y-direction.
+	D3DXVECTOR3 vEyePt(0.0f, 0.0f, -12.0f);
+	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
+	D3DXMATRIXA16 matView;
+	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
+	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
 
-    // For the projection matrix, we set up a perspective transform (which
-    // transforms geometry from 3D view space to 2D viewport space, with
-    // a perspective divide making objects smaller in the distance). To build
-    // a perpsective transform, we need the field of view (1/4 pi is common),
-    // the aspect ratio, and the near and far clipping planes (which define at
-    // what distances geometry should be no longer be rendered).
-    D3DXMATRIXA16 matProj;
-    D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f );
-    g_pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+	// For the projection matrix, we set up a perspective transform (which
+	// transforms geometry from 3D view space to 2D viewport space, with
+	// a perspective divide making objects smaller in the distance). To build
+	// a perpsective transform, we need the field of view (1/4 pi is common),
+	// the aspect ratio, and the near and far clipping planes (which define at
+	// what distances geometry should be no longer be rendered).
+	D3DXMATRIXA16 matProj;
+	D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f);
+	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
+
 }
 
 VOID SetupMatrices2()
@@ -431,33 +380,187 @@ VOID Render()
     // Begin the scene
     if( SUCCEEDED( g_pd3dDevice->BeginScene() ) )
     {
+		static bool go_left;
+		static bool go_right;
+		static bool moving;
+		static short stat = 0;
+		static bool increase = true;
+
+		static float upvel = 0;
+		static bool jumping_up = false;
+		static bool jumping = false;
+		static bool jumping_tgl = false;
+
+		static float sliding_vel = 0;
+		static short sliding = 0;
+		static short sliding_rt = 0;
+		static bool sliding_tgl = false;
+
+		moving = false;
+
+		SetupMatrices();
+
+		if (sliding == 0)
+		{
+			if (KEY_DOWN(VK_LEFT)){
+				go_left = true; go_right = false; moving = true;
+			}
+			if (KEY_DOWN(VK_RIGHT)) {
+				go_right = true; go_left = false; moving = true;
+			}
+			if (KEY_DOWN(VK_UP)) {
+				if (!jumping&&!jumping_tgl) {
+					jumping_tgl = true;
+					jumping = true; jumping_up = true; upvel = 6.0f;
+				}
+				else if (jumping) {
+					if (jumping_up)
+						upvel += 0.495;
+				}
+			}
+			if (jumping_tgl) { if (KEY_UP(VK_UP)) jumping_tgl = false; }
+			if (KEY_DOWN(VK_DOWN)&&KEY_DOWN(VK_RIGHT)|| KEY_DOWN(VK_DOWN) && KEY_DOWN(VK_LEFT)) {
+				if (!sliding_tgl&&!jumping) {
+					sliding_tgl = true;
+					dustpos = pos; dustpos.y += 43;
+					if(go_left)dustpos.x -= 60;
+					
+					sliding_rt = 15;
+					moving = false;
+					sliding_vel = 8;
+					if (go_right) sliding = 1;/*right*/
+					else if (go_left)  sliding = 2;/*left*/
+				}
+			}
+			if (sliding_tgl) { if (KEY_UP(VK_DOWN)) sliding_tgl = false; }
+		}
+
+		if (go_left&&go_right)
+			moving = false;
+
+		if (!moving&&!jumping&&sliding == 0)
+		{
+			g_ImgRc1.bottom = 64;
+			if (go_right) {
+				g_ImgRc1.left = 0;
+				g_ImgRc1.right = g_ImgRc1.left + 49;
+			}
+			else if (go_left) {
+				g_ImgRc1.left = 350;
+				g_ImgRc1.right = g_ImgRc1.left + 49;
+			}
+			else if (!go_right&&!go_left)
+			{
+				g_ImgRc1.left = 0;
+				g_ImgRc1.right = g_ImgRc1.left + 49;
+			}
+		}
+		else if (moving&&!jumping)
+		{
+			g_ImgRc1.bottom = 64;
+			if (go_right)
+			{
+				if (stat >= 0 && stat <= 7) {
+					g_ImgRc1.left = 50;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+				else if (stat >= 8 && stat <= 23) {
+					g_ImgRc1.left = 100;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+				else if (stat >= 24 && stat <= 31) {
+					g_ImgRc1.left = 150;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+			}
+			else if (go_left)
+			{
+				if (stat >= 0 && stat <= 7) {
+					g_ImgRc1.left = 300;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+				else if (stat >= 8 && stat <= 23) {
+					g_ImgRc1.left = 250;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+				else if (stat >= 24 && stat <= 31) {
+					g_ImgRc1.left = 200;
+					g_ImgRc1.right = g_ImgRc1.left + 49;
+				}
+			}
+		}
+		else if (jumping) {
+			g_ImgRc1.bottom = 64;
+			if (go_right) {
+				g_ImgRc1.left = 400;
+				g_ImgRc1.right = g_ImgRc1.left + 59;
+			}
+			else if (go_left) {
+				g_ImgRc1.left = 460;
+				g_ImgRc1.right = g_ImgRc1.left + 59;
+			}
+		}
+		else if (sliding) {
+			if (go_right) {
+				g_ImgRc1.left = 520;
+				g_ImgRc1.right = g_ImgRc1.left + 59;
+			}
+			else if (go_left) {
+				g_ImgRc1.left = 580;
+				g_ImgRc1.right = g_ImgRc1.left + 59;
+			}
+		}
+		if (stat >= 31) increase = false; else if (stat <= 0)increase = true;
+		if (increase)stat++; else stat--;
+		if (moving)
+		{
+			if (go_left) pos.x -= 2.0f; if (go_right) pos.x += 2.0f;
+		}
+		else if (sliding != 0)
+		{
+			if (go_left) pos.x -= sliding_vel; if (go_right) pos.x += sliding_vel;
+			sliding_vel -= 0.45f;
+			if (sliding_rt >= 4) {
+				if (go_right) {
+					g_ImgRcDust.left = ((15 - sliding_rt) * 120) % 720;
+					g_ImgRcDust.right = g_ImgRcDust.left + 119;
+					if (9 >= sliding_rt)g_ImgRcDust.top = 20;
+					else g_ImgRcDust.top = 0;
+					g_ImgRcDust.bottom = g_ImgRcDust.top + 19;
+				}
+				else if (go_left) {
+					g_ImgRcDust.right = 720-((15 - sliding_rt) * 120) % 720;
+					g_ImgRcDust.left = g_ImgRcDust.right - 119;
+					if (9 >= sliding_rt)g_ImgRcDust.top = 60;
+					else g_ImgRcDust.top = 40;
+					g_ImgRcDust.bottom = g_ImgRcDust.top + 19;
+				}
+				
+			}
+			sliding_rt--; if (sliding_rt == -1) sliding = 0;
+		}
+		if (jumping)
+		{
+			if (upvel <= 0) jumping_up = false;
+			if (pos.y - upvel >= MIN_Y) { pos.y = MIN_Y; jumping = false; /*먼지 스프라이트 추가*/ }
+			else { pos.y -= upvel; upvel -= ACC_GRV; }
+		}
+
         // Setup the world, view, and projection matrices
-		SetupMatrices2();
 
-		// Setup our texture. Using Textures introduces the texture stage states,
-		// which govern how Textures get blended together (in the case of multiple
-		// Textures) and lighting information. In this case, we are modulating
-		// (blending) our texture with the diffuse color of the vertices.
-	
-		g_pd3dDevice->SetTexture(0, g_pTexture2);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_DISABLE);
-
-		// Render the vertex buffer contents
-		g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
-		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 4, 2);
-
-        SetupMatrices();
 
 		
-		
-
+		g_pd3dSpritebg->Begin(D3DXSPRITE_ALPHABLEND);
+		g_pd3dSpritebg->Draw(g_pTexturebg, &g_ImgRcbg,NULL, &D3DXVECTOR3(0, 0, 0), D3DXCOLOR(1, 1, 1, 1.0f));
+		g_pd3dSpritebg->End();
 		g_pd3dSprite->Begin(D3DXSPRITE_ALPHABLEND);
-		g_pd3dSprite->Draw(g_pTexture, &g_ImgRc1, NULL, &pos, D3DXCOLOR(1, 1, 1, 1.0F)); // center is NULL
+		g_pd3dSprite->Draw(g_pTexture, &g_ImgRc1, NULL, &pos, D3DXCOLOR(1, 1, 1, 1.0f)); // center is NULL
 		g_pd3dSprite->End();
+		if (sliding) {
+			g_pd3dSpriteDust->Begin(D3DXSPRITE_ALPHABLEND);
+			g_pd3dSpriteDust->Draw(g_pTextureDust, &g_ImgRcDust, NULL, &dustpos, D3DXCOLOR(1, 1, 1, 0.6f));
+			g_pd3dSpriteDust->End();
+		}
         // End the scene
         g_pd3dDevice->EndScene();
     }
@@ -508,7 +611,7 @@ INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
 
     // Create the application's window
     HWND hWnd = CreateWindow( L"D3D Tutorial", L"D3D Tutorial 05: Textures",
-                              WS_OVERLAPPEDWINDOW, 100, 100, 300, 300,
+                              WS_OVERLAPPEDWINDOW, 200, 200, 525, 515,
                               NULL, NULL, wc.hInstance, NULL );
 
     // Initialize Direct3D
